f75ca4bccfe2384722a444a14dddbf4a23858ed7
JuanjoEG <juanjoestevezgonzalez@gmail.com>
2022-10-15 22:11:56 +0200

    Version 3

---
+++ src/Tema_03/Excepciones.java
@@ -0,0 +1,127 @@
+package Tema_03;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Juan José Estévez González
+ */
+public class Excepciones {
+    
+    public static void main(String[] args) {
+        
+        String menu;
+        do {            
+            Scanner teclaStr = new Scanner(System.in);
+            System.out.println("""                          
+                               
+                               
+                               *****************************************************************************
+                               *                                EXCEPCIONES                                *
+                               *****************************************************************************
+                               *  TRES TIPOS:                                                              *
+                               *                                                                           *
+                               *       - CHECKED EXCEPTIONS (CON CHEQUEO)                                  *
+                               *                            DURANTE LA COMPILACIÓN                         *
+                               *                                                                           *
+                               *       - UNCHECKED EXCEPTIONS (SIN CHEQUEO)  RunTimeExceptionS             *
+                               *                            EN TIEMPO DE EJECUCIÓN                         *
+                               *                                                                           *
+                               *       - ERRORES (FUERA DEL CONTROL DEL USUARIO Y DESARROLLADOR)           *
+                               *                                                                           *
+                               *****************************************************************************
+                               *   TODAS HEREDAN DE LA CLASE  -->  Throwable  (SUPERCLASE)                 *
+                               *                                    | Exception  (SUBCLASE)                *
+                               *                                    | Error      (SUBCLASE)                *
+                               *                                                                           *
+                               *   Throwable IMPLEMENTA LA INTERFAZ SERIALIZABLE                           *
+                               *****************************************************************************
+                               *   LOS MÉTODOS PRINCIPALES PARA GESTIONAR EXCEPCIONE:                      *
+                               *                                                                           *
+                               *   getMessage()       -> DEVUELVE MENSAJE DETALLADO SOBRE LA EXCEPCIÓN     *
+                               *   getCause()         -> DEVELVE LA CAUSA EN UN OBJETO Throwable           *
+                               *   toString()         -> DEVUELVE EL NOMBRE + getMessage()                 *
+                               *   printStackTrace()  -> IMPRIME toString() + ERROR DE LA PILA             *
+                               *   getStackTrace()    -> DEVUELVE UN ARRAY CON LOS ELEMENTOS DE LA PILA    *
+                               *   fillInstackTrace() -> RELLENA LA PILA DEL OBJETO Throwable              *
+                               *****************************************************************************
+                               *   FORMAS DE TRATAR UNA EXCEPCIÓN:                                         *
+                               *   1º- AÑADIR LA EXCEPCIÓN A LA DEFINICIÓN DEL MÉTODO -> PASA A OTRO NIVEL.*
+                               *   2º- CONTRÓL MEDIANTE LOS BLOQUES  -->  try-catch                        *
+                               *       LOS BLOQUES  try-catch  SE PONDRÁN EN EL PROGRAMA PRINCIPAL.        *
+                               *****************************************************************************
+                               *   SI LA EXCEPCIÓN SE DA EN UN MÉTODO  -->  AÑADIR  throws  Y...           *
+                               *   LA EJECUCIÓN SE VA AL catsh DEL PROGRAMA PRINCIPAL.                     *
+                               *****************************************************************************
+                               *          SI QUEREMOS LANZAR NOSOTROS UNA EXCEPCIÓN:                       *
+                               *   thow new MiException(); --> PODEMOS PERSONALIZAR LA EXCEPCIÓN.          *
+                               *****************************************************************************
+                               *   EN EL PROGRAMA PRINCIPAL:                                               *
+                               *                                                                           *
+                               *    try {                                                                  *
+                               *        BLOQUE QUE PUEDE GENERAR LA EXCEPCIÓN;                             *
+                               *        CÓDIGO PROTEGIDO;                                                  *
+                               *    } catch (TipoException e) {                                            *
+                               *        BLOQUE QUE SE MUESTRA SI HAY UNA EXCEPCIÓN;                        *
+                               *        e.printStackTrace();                                               *
+                               *        System.out.println("Error: " + e.toString());                      *
+                               *        System.out.println("Error: " + e.getMessage());                    *
+                               *        System.out.println("La Excepción es: " + e.getClass());            *
+                               *    } finaly {                                                             *
+                               *        BLOQUE QUE SIEMPRE SE EJECUTA;                                     *
+                               *        CIERRE DE RECURSOS.                                                *
+                               *    }                                                                      *
+                               *                                                                           *
+                               *****************************************************************************
+                               *   UN try    -->  PUEDE TENER VARIOS catch  -->  MULTICATCH                *
+                               *   UN catch  -->  TIENE QUE TENER UN try OBLIGATORIAMENTE                  *
+                               *****************************************************************************
+                               *****************************************************************************
+                               *                       EXCEPCIONES PERSONALIZADAS                          *
+                               *****************************************************************************
+                               *   SU NOMBRE DEBERÁ TERMINAR EN  ...Exception                              *
+                               *   TENDRÁ UN CONSTRUCTOR QUE LE LLEGA UN String COMO PARÁMETRO.            *
+                               *   String  -->  MENSAJE DE NUESTRO ERROR PERSONALIZADO.                    *
+                               *****************************************************************************
+                               *   SI SE DAN LAS CONDICIONES REQUERIDAS DE LA EXCEPCIÓN ...                *
+                               *   SE LANZA NUESTRA EXCEPCIÓN CON EL CÓDIGO:                               *
+                               *                                                                           *
+                               *   throw new TipoException(String);                                        *
+                               *                                                                           *
+                               *****************************************************************************
+                               *   SI LA EXCEPCIÓN SE DA EN UN MÉTODO  -->  AÑADIR  throws  Y...           *
+                               *   LA EJECUCIÓN SE VA AL catsh DEL PROGRAMA PRINCIPAL.                     *
+                               *                                                                           *
+                               *   private static void unMetodo() throws TipoException {                   *
+                               *        ...                                                                *
+                               *        if (CONDICIÓN) {                                                   *
+                               *            throw new TipoException("¡¡¡ No es correcto !!!");             *
+                               *        }                                                                  *
+                               *        ...                                                                *
+                               *   }                                                                       *
+                               *                                                                           *
+                               *****************************************************************************
+                               *   EN LA CLASE EXCEPCIÓN PERSONALIZADA:                                    *
+                               *                                                                           *
+                               *  public class TipoException extends Exception {                           *
+                               *                                                                           *
+                               *      public TipoException (String mensajeError) {                         *
+                               *          super(mensajeError);                                             *
+                               *      }                                                                    *
+                               *  }                                                                        *
+                               *                                                                           *
+                               *****************************************************************************
+                               *  0. ATRÁS.                                                                *
+                               *****************************************************************************
+                                                              
+                               
+                               """);
+            System.out.print("Seleccione una opción: ");
+            menu = teclaStr.nextLine();
+            switch(menu){
+                case "0" -> {}
+                default -> System.out.println("La opción no es correcta.");
+            }
+        } while (!"0".equals(menu));
+    }
+}
---
+++ src/Tema_03/ParsersXPath.java
@@ -0,0 +1,96 @@
+package Tema_03;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
+import javax.xml.xpath.XPathExpressionException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+
+import org.xml.sax.SAXException;
+
+/**
+ *
+ * @author Juan José Estévez González
+ */
+public class ParsersXPath {
+    
+    public static void main(String[] args) {
+        
+        String ruta = "J:\\Ejercicios\\inputxml.txt";
+        
+        try {
+            
+            File inputFile = new File(ruta);
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            
+            Document doc = dBuilder.parse(inputFile);
+            doc.getDocumentElement().normalize();
+            
+            XPath xPath = XPathFactory.newInstance().newXPath();
+            
+            String expresion = "/class/student";
+            NodeList nodeList = (NodeList) xPath.compile(expresion).evaluate(doc, XPathConstants.NODESET);
+            
+            for (int i=0; i<nodeList.getLength(); i++) {
+            
+                Node nNode = nodeList.item(i);
+                System.out.println("\nElemento actual : " + nNode.getNodeName());
+                
+                if(nNode.getNodeType() == Node.ELEMENT_NODE) {
+                
+                    Element eElement = (Element) nNode;
+                    System.out.println("Número de Estudiante : " + eElement.getAttribute("rollno"));
+                    System.out.println("Nombre : "
+                    
+                            + eElement
+                            .getElementsByTagName("firstname")
+                            .item(0)
+                            .getTextContent()
+                    );
+                    System.out.println("Apellido : "
+                    
+                            + eElement
+                            .getElementsByTagName("lastname")
+                            .item(0)
+                            .getTextContent()
+                    );
+                    System.out.println("Apodo : "
+                    
+                            + eElement
+                            .getElementsByTagName("nickname")
+                            .item(0)
+                            .getTextContent()
+                    );
+                    System.out.println("Marca : "
+                    
+                            + eElement
+                            .getElementsByTagName("marks")
+                            .item(0)
+                            .getTextContent()
+                    );
+                }                
+            }
+        }catch (ParserConfigurationException e) {        
+            e.printStackTrace();
+        }catch (SAXException e) {        
+            e.printStackTrace();
+        }catch (IOException e) {        
+            e.printStackTrace();
+        } catch (XPathExpressionException e) {        
+            e.printStackTrace();
+        }
+    }    
+}
